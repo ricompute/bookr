d8fc3a52410f9b14683b27fa1471f21ce96acb82
Rick Lindsey <richard.c.lindsey@ricompute.com>
2016-01-11 14:55:19 -0800

    Add read_booklog function

--- R/read_booklog.R
+++ R/read_booklog.R
@@ -1,7 +1,7 @@
 #' Read booklog.csv into a Data Frame
 #'
 #' This is just a wrapper around read.csv to read booklog.csv into a data frame.
-#' @param file A character string containing the file to be read in, or a file connection. Defaults to booklog.csv.
+#' @param file A character string containing the name of the file to be read in, or a file connection. Defaults to booklog.csv.
 #' @param ... Further arguments to be passed to read.csv.
 #'
 #' @return Returns a data frame with the contents of booklog.csv.
