fe339c4808117cb4b198d3493d7a1f3cc2caf1f1
Rick Lindsey <richard.c.lindsey@ricompute.com>
2016-01-11 14:12:09 -0800

    Check that rating is between 1 and 10, and add comments.

--- R/booklog.R
+++ R/booklog.R
@@ -14,23 +14,34 @@
 
 booklog <- function(title, author = NA, date = Sys.Date(), rating = NA, notes = NA) {
 
+        # Create a booklog.csv if one doesn't already exist in the working directory
         if (!file.exists("booklog.csv")) {
                 file.create("booklog.csv")
-                new_file = TRUE
+                message("A new booklog.csv has been created in the current working directory.")
+                new_file = TRUE  # Note that this is a new file
         } else {
                 new_file = FALSE
         }
 
+        # Warn the user if they did not enter an author
         if (is.na(author)) {
-                warning("Did you really want to enter a book with no author?")
+                warning("Did you really want to enter a book with no author? Author has been recorded as NA.")
         }
 
+        # Check to make sure the rating is a number between 1 and 10 (or NA)
+        if (rating < 1 | rating > 10 | (!is.numeric(rating) & !is.na(rating))) {
+                rating <- NA
+                warning("The rating you entered was not a number between 1 and 10. It has been recorded as NA.")
+        }
+
+        # Create a data frame of the book data to be written to booklog.csv
         to_write <- data.frame(Book = title,
                                Author = author,
                                Date = as.character(date),
                                Rating = as.character(rating),
                                Notes = notes)
 
+        # Write the book data to booklog.csv
         write.table(to_write, file = "booklog.csv", append = !new_file, sep = ",",
                     row.names = FALSE, col.names = new_file)
 
